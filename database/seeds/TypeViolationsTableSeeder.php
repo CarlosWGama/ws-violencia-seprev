ec878b97c4c60c29467444c1ca69d25b6a9190dc
Carlos W. Gama <carloswgama@gmail.com>
2020-07-15 15:11:53 -0300

    Alterado Tipos de Denuncias

--- database/seeds/TypeViolationsTableSeeder.php
+++ database/seeds/TypeViolationsTableSeeder.php
@@ -12,25 +12,18 @@ class TypeViolationsTableSeeder extends Seeder
      */
     public function run()
     {
-        ViolationType::create([
-            'id' => 1,
-            'descricao' => "Venda Bebidas Alcoolicas"
-        ]);
-        ViolationType::create([
-            'id' => 2,
-            'descricao' => "Bullying"
-        ]);
-        ViolationType::create([
-            'id' => 3,
-            'descricao' => "Trabalho Infantil"
-        ]);
-        ViolationType::create([
-            'id' => 4,
-            'descricao' => "Exploracao Sexual"
-        ]);
-        ViolationType::create([
-            'id' => 5,
-            'descricao' => "Automutilacao"
-        ]);
+        ViolationType::create(['id' => 1,'descricao' => "Maus-tratos "]);
+        ViolationType::create(['id' => 2,'descricao' => "Negligência"]);
+        ViolationType::create(['id' => 3,'descricao' => "Fornecimento de bebida alcoólica"]);
+        ViolationType::create(['id' => 4,'descricao' => "Fornecimento de armas, munição ou explosivos"]);
+        ViolationType::create(['id' => 5,'descricao' => "Hospedagem Irregular"]);
+        ViolationType::create(['id' => 6,'descricao' => "Tráfico de drogas"]);
+        ViolationType::create(['id' => 7,'descricao' => "Transporte Irregular"]);
+        ViolationType::create(['id' => 8,'descricao' => "Trabalho Infantil"]);
+        ViolationType::create(['id' => 9,'descricao' => "Violência Física"]);
+        ViolationType::create(['id' => 10,'descricao' => "Violência Psicológica"]);
+        ViolationType::create(['id' => 11,'descricao' => "Violência Sexual"]);
+        ViolationType::create(['id' => 12,'descricao' => "Violência Institucional"]);
+        ViolationType::create(['id' => 13,'descricao' => "Violência Autoprovocada/Autoinfligida"]);
     }
 }
